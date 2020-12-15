bff1e883a6f921d3f9a2bff3d3d1e9802175bb9a
mateuszpsnik <matikolbudy@gmail.com>
2020-12-15 23:41:00 +0100

    Simple routing (to an article)

--- Node.js/app.js
+++ Node.js/app.js
@@ -1,22 +1,40 @@
 /*jshint esversion: 8*/
 
+const { render } = require("ejs");
 const express = require("express");
 const app = express();
 const path = require("path");
 
 require("./connection");
 
-const data = require("./data");
-
-(async () => {
-    console.log(await data.articles());
-})();
-
 app.set("view engine", "ejs");
 
 app.listen("3000");
 app.use(express.static(__dirname + "/public"));
 
+const Article = require("./models/Article");
+
 app.get("/", (req, res) => { 
-    res.render("index");
+    res.redirect("/articles");
+});
+
+app.get("/articles", (req, res) => {
+    Article.findAll()
+    .then((result) => {
+        res.render("index", { title: "Wszystkie artykuły", articles: result });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get("/articles/:id", (req, res) => {
+    const id = req.params.id;
+    Article.findAll({ where: { id: id } })
+    .then(result => {
+        res.render("article", { title: result[0].title, article: result[0] });
+    })
+    .catch(err => {
+        console.log(err);
+    });
 });
